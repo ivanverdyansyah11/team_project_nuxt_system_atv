9153f30f2d9d892d3d73b796e7c664907999253e
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-07-13 00:14:10 +0800

    Progress: Finish update index dashboard page

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,20 +1,59 @@
 <script setup lang="ts">
-import {navigateTo} from "nuxt/app";
-import {useAuthStore} from "~/stores/auth";
-
-const authStore = useAuthStore();
-const logout = async (event: any) => {
-  event.preventDefault();
-  await authStore.logout();
-  navigateTo('/login');
-};
+definePageMeta({
+  title: 'Dashboard Page',
+  layout: 'dashboard'
+})
 </script>
 
 <template>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, nemo!</p>
-  <form @submit.prevent="logout">
-    <button type="submit">Logout</button>
-  </form>
+  <div class="content container mt-4">
+    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-3 g-lg-4">
+      <div class="col">
+        <div class="card-menu d-flex align-items-center gap-3">
+          <div class="menu-icon d-flex align-items-center justify-content-center">
+            <i class="fa-solid fa-users"></i>
+          </div>
+          <div class="menu-value">
+            <p>Total Customer</p>
+            <h6>12</h6>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card-menu d-flex align-items-center gap-3">
+          <div class="menu-icon d-flex align-items-center justify-content-center">
+            <i class="fa-regular fa-id-card"></i>
+          </div>
+          <div class="menu-value">
+            <p>Total Driver</p>
+            <h6>10</h6>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card-menu d-flex align-items-center gap-3">
+          <div class="menu-icon d-flex align-items-center justify-content-center">
+            <i class="fa-solid fa-money-bill-transfer"></i>
+          </div>
+          <div class="menu-value">
+            <p>Total Transaction</p>
+            <h6>4</h6>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card-menu d-flex align-items-center gap-3">
+          <div class="menu-icon d-flex align-items-center justify-content-center">
+            <i class="fa-solid fa-coins"></i>
+          </div>
+          <div class="menu-value">
+            <p>Total Income</p>
+            <h6>Rp. 100.000</h6>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
