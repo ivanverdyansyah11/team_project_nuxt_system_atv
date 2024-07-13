e38706c20086adf688f4468b8ff46818f676e0bb
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-07-14 00:54:32 +0800

    Progress: Update auth middleware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,24 +1,23 @@
-import { defineNuxtRouteMiddleware } from 'nuxt/app'
-import { useAuthStore } from '~/stores/auth'
-import { navigateTo } from 'nuxt/app'
+import { defineNuxtRouteMiddleware } from 'nuxt/app';
+import { useAuthStore } from '~/stores/auth';
+import { navigateTo } from 'nuxt/app';
 
-export default defineNuxtRouteMiddleware((to, from) => {
-    const nuxt = useNuxtApp()
-    const authStore = useAuthStore()
-    const authToken = useCookie('auth-token')
-    const authUser = useCookie('auth-user')
+export default defineNuxtRouteMiddleware(async (to, from) => {
+    const authStore = useAuthStore();
+    const authToken = useCookie('auth-token');
+    const authUser = useCookie('auth-user');
 
-    if(authToken.value) {
-        authStore.token = authToken.value
+    if (authToken.value) {
+        authStore.token = authToken.value;
     }
 
-    if(authUser.value) {
-        authStore.user = authUser.value
+    if (authUser.value) {
+        authStore.user = authUser.value;
     }
 
     if (!authStore.isLoggedIn && (to.path === '/dashboard')) {
-        return navigateTo('/dashboard/auth/login')
+        return navigateTo('/dashboard/auth/login');
     } else if (authStore.isLoggedIn && (to.path === '/dashboard/auth/login' || to.path === '/dashboard/auth/register')) {
-        return navigateTo('/dashboard')
+        return navigateTo('/dashboard');
     }
-})
+});
