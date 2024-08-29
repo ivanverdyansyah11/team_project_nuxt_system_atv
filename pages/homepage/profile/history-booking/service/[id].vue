a601d0964ba2373b722c83bbf9c918c80556c510
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-08-29 20:06:01 +0800

    Progress: Update homepage profile history booking service detail page

--- pages/homepage/profile/history-booking/service/[id].vue
+++ pages/homepage/profile/history-booking/service/[id].vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {useBookingStore} from "~/stores/booking"
+import {useBookingServiceStore} from "~/stores/bookingService"
 import {useRoute} from '#app'
 import {formatRupiah} from "~/helpers/FormatRupiah"
 import {formatDate} from "~/helpers/FormatDate"
@@ -8,16 +8,16 @@ definePageMeta({
   layout: 'profile',
 })
 
-const bookingStore = useBookingStore()
+const bookingServiceStore = useBookingServiceStore()
 const route = useRoute()
 
-await bookingStore.getBookingById(route.params.id)
+await bookingServiceStore.getBookingServiceById(route.params.id)
 </script>
 
 <template>
   <form class="form w-100">
     <div class="row g-3">
-      <div class="col-12" v-for="(service, index) in bookingStore.booking.details" :key="index">
+      <div class="col-12" v-for="(service, index) in bookingServiceStore.bookingService.details" :key="index">
         <div class="row">
           <div class="col-md-4">
             <div class="input-group d-flex flex-column">
@@ -33,8 +33,8 @@ await bookingStore.getBookingById(route.params.id)
           </div>
           <div class="col-md-4">
             <div class="input-group d-flex flex-column">
-              <label for="price">Price</label>
-              <input type="text" class="input w-100" name="price" id="price" :value="formatRupiah(service.price)" readonly>
+              <label for="unit_price">Unit Price</label>
+              <input type="text" class="input w-100" name="unit_price" id="unit_price" :value="formatRupiah(service.entertainment_service.price)" readonly>
             </div>
           </div>
         </div>
@@ -42,53 +42,53 @@ await bookingStore.getBookingById(route.params.id)
       <div class="col-md-6">
         <div class="input-group d-flex flex-column">
           <label for="code">Code Booking</label>
-          <input type="text" class="input w-100" name="code" id="code" v-model="bookingStore.booking.code" readonly>
+          <input type="text" class="input w-100" name="code" id="code" v-model="bookingServiceStore.bookingService.code" readonly>
         </div>
       </div>
       <div class="col-md-6">
         <div class="input-group d-flex flex-column">
           <label for="date">Booking Date</label>
-          <input type="text" class="input w-100" name="date" id="date" :value="formatDate(bookingStore.booking.date)" readonly>
+          <input type="text" class="input w-100" name="date" id="date" :value="formatDate(bookingServiceStore.bookingService.date)" readonly>
         </div>
       </div>
       <div class="col-md-6">
         <div class="input-group d-flex flex-column">
           <label for="customer">Customer</label>
-          <input type="text" class="input w-100" name="customer" id="customer" v-model="bookingStore.booking.name" readonly>
+          <input type="text" class="input w-100" name="customer" id="customer" v-model="bookingServiceStore.bookingService.name" readonly>
         </div>
       </div>
       <div class="col-md-6">
         <div class="input-group d-flex flex-column">
           <label for="phone_number">Phone Number</label>
-          <input type="text" class="input w-100" name="phone_number" id="phone_number" v-model="bookingStore.booking.phone_number" readonly>
+          <input type="text" class="input w-100" name="phone_number" id="phone_number" v-model="bookingServiceStore.bookingService.phone_number" readonly>
         </div>
       </div>
       <div class="col-md-6">
         <div class="input-group d-flex flex-column">
           <label for="payment_method">Payment Method</label>
-          <input type="text" class="input w-100" name="payment_method" id="payment_method" v-model="bookingStore.booking.payment_method" readonly>
+          <input type="text" class="input w-100" name="payment_method" id="payment_method" v-model="bookingServiceStore.bookingService.payment_method" readonly>
         </div>
       </div>
       <div class="col-md-6">
         <div class="input-group d-flex flex-column">
           <label for="total_price">Total Price</label>
-          <input type="text" class="input w-100" name="total_price" id="total_price" :value="formatRupiah(bookingStore.booking.total_price)" readonly>
+          <input type="text" class="input w-100" name="total_price" id="total_price" :value="formatRupiah(bookingServiceStore.bookingService.total_price)" readonly>
         </div>
       </div>
       <div class="col-md-6">
         <div class="input-group d-flex flex-column">
           <label for="total_pay">Total Pay</label>
-          <input type="text" class="input w-100" name="total_pay" id="total_pay" :value="formatRupiah(bookingStore.booking.total_pay)" readonly>
+          <input type="text" class="input w-100" name="total_pay" id="total_pay" :value="formatRupiah(bookingServiceStore.bookingService.total_pay)" readonly>
         </div>
       </div>
       <div class="col-md-6">
         <div class="input-group d-flex flex-column">
           <label for="total_change">Total Change</label>
-          <input type="text" class="input w-100" name="total_change" id="total_change" :value="formatRupiah(bookingStore.booking.total_change)" readonly>
+          <input type="text" class="input w-100" name="total_change" id="total_change" :value="formatRupiah(bookingServiceStore.bookingService.total_change)" readonly>
         </div>
       </div>
       <div class="col-12 d-flex gap-2 align-items-center">
-        <NuxtLink :to="{path: '/homepage/profile/history-booking'}" class="button-reverse">Back to History Page</NuxtLink>
+        <NuxtLink :to="{path: '/homepage/profile/history-booking/service'}" class="button-reverse">Back to History Page</NuxtLink>
       </div>
     </div>
   </form>
