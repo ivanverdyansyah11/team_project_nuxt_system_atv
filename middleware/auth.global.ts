0b8226863cce2dba017ce4b48bc74f7500fe8154
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-08-29 09:37:21 +0800

    Progress: Update middleware auth global

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -15,9 +15,19 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
         authStore.user = authUser.value;
     }
 
-    if (!authStore.isLoggedIn && (to.path === '/dashboard')) {
-        return navigateTo('/dashboard/auth/login');
-    } else if (authStore.isLoggedIn && (to.path === '/dashboard/auth/login' || to.path === '/dashboard/auth/register')) {
+    if (!authStore.isLoggedIn && (to.path.startsWith('/dashboard') || to.path.startsWith('/homepage/profile') || to.path.startsWith('/homepage/booking'))) {
+        return navigateTo('/homepage');
+    } else if (authStore.isLoggedIn && (to.path === '/auth/login' || to.path === '/auth/register')) {
+        return navigateTo('/homepage');
+    } else if (authStore.isLoggedIn && authUser.value.user.role != 'customer' && to.path.startsWith('/homepage/profile')) {
+        return navigateTo('/homepage');
+    } else if (authStore.isLoggedIn && authUser.value.user.role == 'instructor' && to.path.startsWith('/homepage/booking')) {
+        return navigateTo('/homepage');
+    } else if (authStore.isLoggedIn && authUser.value.user.role == 'customer' && to.path.startsWith('/dashboard')) {
+        return navigateTo('/homepage');
+    } else if (authStore.isLoggedIn && authUser.value.user.role == 'instructor' && (to.path.startsWith('/dashboard/instructor') || to.path.startsWith('/dashboard/staff') || to.path.startsWith('/dashboard/customer') || to.path.startsWith('/dashboard/facility') || to.path.startsWith('/dashboard/route') || to.path.startsWith('/dashboard/luggage') || to.path.startsWith('/dashboard/blog') || to.path.startsWith('/dashboard/booking') || to.path == '/dashboard/entertainment-package/create' || to.path.startsWith('/dashboard/entertainment-package/edit') || to.path == '/dashboard/entertainment-service/create' || to.path.startsWith('/dashboard/entertainment-service/edit'))) {
+        return navigateTo('/dashboard');
+    } else if (authStore.isLoggedIn && authUser.value.user.role == 'staff' && (to.path.startsWith('/dashboard/instructor') || to.path.startsWith('/dashboard/staff') || to.path.startsWith('/dashboard/customer') || to.path.startsWith('/dashboard/facility') || to.path.startsWith('/dashboard/route') || to.path.startsWith('/dashboard/luggage') || to.path == '/dashboard/entertainment-package/create' || to.path.startsWith('/dashboard/entertainment-package/edit') || to.path == '/dashboard/entertainment-service/create' || to.path.startsWith('/dashboard/entertainment-service/edit'))) {
         return navigateTo('/dashboard');
     }
 });
