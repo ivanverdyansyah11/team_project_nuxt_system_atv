968fc24abced3480c42b3596f8d213b5d03347c0
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-08-29 20:05:36 +0800

    Progress: Update stores booking package

--- stores/bookingPackage.ts
+++ stores/bookingPackage.ts
@@ -2,10 +2,10 @@ import {defineStore} from 'pinia'
 import {apiUrl} from '~/helpers/Variable'
 import Cookies from "js-cookie"
 
-export const useBookingStore = defineStore('booking', {
+export const useBookingPackageStore = defineStore('bookingPackage', {
     state: () => ({
-        bookingAll: [],
-        booking: {},
+        bookingPackageAll: [],
+        bookingPackage: {},
         keyword: ref(''),
         page: 1,
         pageSize: 6,
@@ -13,94 +13,77 @@ export const useBookingStore = defineStore('booking', {
         status_code: null,
     }),
     actions: {
-        async getAllBookingWithoutPaginate() {
+        async getAllBookingPackageWithoutPaginate() {
             try {
                 const token = useCookie('auth-token')
                 const user = useCookie('auth-user')
-                const path = ref(`${ apiUrl }/bookings`)
+                const path = ref(`${ apiUrl }/booking/packages?search=${this.keyword}`)
                 if (user.value.user.role == 'customer') {
-                    path.value = `${apiUrl}/bookings?customer_id=${user.value.id}`
+                    path.value = `${apiUrl}/booking/packages?customer_id=${user.value.id}&search=${this.keyword}`
                 } else if(user.value.user.role == 'instructor') {
-                    path.value = `${apiUrl}/bookings?instructor_id=${user.value.id}`
+                    path.value = `${apiUrl}/booking/packages?instructor_id=${user.value.id}&search=${this.keyword}`
                 }
                 const response = await $fetch( path.value, {
                     method: 'GET',
                     headers: { 'Authorization': `Bearer ${token.value}` },
                 })
-                this.bookingAll = response.data
+                this.bookingPackageAll = response.data
             } catch (error) {
                 this.status_code = error.response.status || error.statusCode || error.code || 'Unknown error'
-                Cookies.set('alert-message', 'Failed to load data booking')
+                Cookies.set('alert-message', 'Failed to load data booking package')
                 Cookies.set('alert-type', 'false')
-                Cookies.set('alert-page', 'Booking')
+                Cookies.set('alert-page', 'Booking Package')
             }
         },
-        async getAllBooking() {
+        async getAllBookingPackage() {
             try {
                 const token = useCookie('auth-token')
                 const user = useCookie('auth-user')
-                const path = ref(`${apiUrl}/bookings?current_page=${this.page}&page_size=${this.pageSize}&search=${this.keyword}`)
+                const path = ref(`${apiUrl}/booking/packages?current_page=${this.page}&page_size=${this.pageSize}&search=${this.keyword}`)
                 if(user.value.user.role == 'instructor') {
-                    path.value = `${apiUrl}/bookings?current_page=${this.page}&page_size=${this.pageSize}&search=${this.keyword}&instructor_id=${user.value.id}`
+                    path.value = `${apiUrl}/booking/packages?current_page=${this.page}&page_size=${this.pageSize}&search=${this.keyword}&instructor_id=${user.value.id}`
                 }
                 const response = await $fetch(path.value, {
                     method: 'GET',
                     headers: { 'Authorization': `Bearer ${token.value}` },
                 })
-                this.bookingAll = response.data
+                this.bookingPackageAll = response.data
                 this.totalPages = response.meta.total
             } catch (error) {
                 this.status_code = error.response.status || error.statusCode || error.code || 'Unknown error'
-                Cookies.set('alert-message', 'Failed to load data booking')
+                Cookies.set('alert-message', 'Failed to load data booking package')
                 Cookies.set('alert-type', 'false')
-                Cookies.set('alert-page', 'Booking')
+                Cookies.set('alert-page', 'Booking Package')
             }
         },
-        async getBookingById(bookingId: string) {
+        async getBookingPackageById(bookingPackageId: string) {
             try {
                 const token = useCookie('auth-token')
-                const response = await $fetch(`${ apiUrl }/bookings/${ bookingId }`, {
+                const response = await $fetch(`${ apiUrl }/booking/packages/${ bookingPackageId }`, {
                     method: 'GET',
                     headers: { 'Authorization': `Bearer ${token.value}` },
                 })
-                this.booking = response.data
+                this.bookingPackage = response.data
             } catch (error) {
                 this.status_code = error.response.status || error.statusCode || error.code || 'Unknown error'
-                Cookies.set('alert-message', 'Failed to load data booking')
+                Cookies.set('alert-message', 'Failed to load data booking package')
                 Cookies.set('alert-type', 'false')
-                Cookies.set('alert-page', 'Booking')
+                Cookies.set('alert-page', 'Booking Package')
             }
         },
-        async exportBooking() {
+        async exportBookingPackage() {
             try {
                 const token = useCookie('auth-token')
-                const response = await $fetch(`${apiUrl}/bookings/export/excel`, {
+                const response = await $fetch(`${apiUrl}/booking/packages/export/excel`, {
                     method: 'GET',
                     headers: { 'Authorization': `Bearer ${token.value}` },
                 })
                 return response
             } catch (error) {
                 this.status_code = error.response.status || error.statusCode || error.code || 'Unknown error'
-                Cookies.set('alert-message', 'Failed to export data booking')
+                Cookies.set('alert-message', 'Failed to export data booking package')
                 Cookies.set('alert-type', 'false')
-                Cookies.set('alert-page', 'Booking')
-            }
-        },
-        async createBookingService(createData: any) {
-            try {
-                const token = useCookie('auth-token')
-                const response = await $fetch(`${apiUrl}/bookings`, {
-                    method: 'POST',
-                    headers: { 'Authorization': `Bearer ${token.value}` },
-                    body: createData
-                })
-                this.status_code = response.meta.code
-                this.totalPages = response.meta.total
-            } catch (error) {
-                this.status_code = error.response.status || error.statusCode || error.code || 'Unknown error'
-                Cookies.set('alert-message', 'Failed to create new booking service')
-                Cookies.set('alert-type', 'false')
-                Cookies.set('alert-page', 'Booking')
+                Cookies.set('alert-page', 'Booking Package')
             }
         },
         async createBookingPackage(createData: any) {
@@ -113,28 +96,29 @@ export const useBookingStore = defineStore('booking', {
                 })
                 this.status_code = response.meta.code
                 this.totalPages = response.meta.total
+                return response.data.id
             } catch (error) {
                 console.log(error.message)
                 this.status_code = error.response.status || error.statusCode || error.code || 'Unknown error'
                 Cookies.set('alert-message', 'Failed to create new booking package')
                 Cookies.set('alert-type', 'false')
-                Cookies.set('alert-page', 'Booking')
+                Cookies.set('alert-page', 'Booking Package')
             }
         },
-        async deleteBooking(bookingId: number) {
+        async deleteBookingPackage(bookingPackageId: number) {
             const token = useCookie('auth-token')
-            const response = await fetch(`${apiUrl}/bookings/${bookingId}`, {
+            const response = await fetch(`${apiUrl}/booking/packages/${bookingPackageId}`, {
                 method: 'DELETE',
                 headers: { 'Authorization': `Bearer ${token.value}` },
             })
             this.status_code = response.status
             if (this.status_code === 200) {
                 this.page = 1
-                await this.getAllBooking()
+                await this.getAllBookingPackage()
             } else {
-                Cookies.set('alert-message', 'Failed to delete booking')
+                Cookies.set('alert-message', 'Failed to delete booking package')
                 Cookies.set('alert-type', 'false')
-                Cookies.set('alert-page', 'Booking')
+                Cookies.set('alert-page', 'Booking Package')
             }
         },
     },
