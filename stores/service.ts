1546fcedc31867093db7b728743c16325fcdbbed
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-08-29 09:23:04 +0800

    Progress: Update stores service

--- stores/service.ts
+++ stores/service.ts
@@ -1,5 +1,6 @@
-import { defineStore } from 'pinia';
-import { apiUrl } from '~/helpers/Variable';
+import {defineStore} from 'pinia'
+import {apiUrl} from '~/helpers/Variable'
+import Cookies from "js-cookie"
 
 export const useServiceStore = defineStore('service', {
     state: () => ({
@@ -16,56 +17,78 @@ export const useServiceStore = defineStore('service', {
         async getAllServiceWithoutPagination() {
             try {
                 const token = useCookie('auth-token')
-                const response = await $fetch(`${ apiUrl }/entertainment/services`, {
+                const response = await $fetch(`${apiUrl}/entertainment/services?&search=${this.keyword}`, {
                     method: 'GET',
                     headers: { 'Authorization': `Bearer ${token.value}` },
                 })
-                this.serviceAll = response?.data ? response?.data : []
+                this.serviceAll = response.data
             } catch (error) {
-                console.log(error?.message)
+                Cookies.set('alert-message', 'Failed to load data entertainment service')
+                Cookies.set('alert-type', 'false')
+                Cookies.set('alert-page', 'Service')
             }
         },
         async getAllService() {
             try {
                 const token = useCookie('auth-token')
-                const response = await $fetch(`${ apiUrl }/entertainment/services?current_page=${this.page}&page_size=${this.pageSize}&search=${this.keyword}`, {
+                const response = await $fetch(`${apiUrl}/entertainment/services?current_page=${this.page}&page_size=${this.pageSize}&search=${this.keyword}`, {
                     method: 'GET',
                     headers: { 'Authorization': `Bearer ${token.value}` },
                 })
-                this.serviceAll = response?.data ? response?.data : []
-                this.totalPages = response?.meta?.total
+                this.serviceAll = response.data
+                this.totalPages = response.meta.total
             } catch (error) {
-                console.log(error?.message)
+                Cookies.set('alert-message', 'Failed to load data entertainment service')
+                Cookies.set('alert-type', 'false')
+                Cookies.set('alert-page', 'Service')
             }
         },
         async getServiceById(serviceId: string) {
             try {
                 const token = useCookie('auth-token')
-                const response = await $fetch(`${ apiUrl }/entertainment/services/${ serviceId }`, {
+                const response = await $fetch(`${apiUrl}/entertainment/services/${ serviceId }`, {
                     method: 'GET',
                     headers: { 'Authorization': `Bearer ${token.value}` },
                 })
-                this.service = response?.data ? response?.data : {}
+                this.service = response.data
             } catch (error) {
-                console.log(error?.message)
+                Cookies.set('alert-message', 'Failed to load data entertainment service')
+                Cookies.set('alert-type', 'false')
+                Cookies.set('alert-page', 'Service')
+            }
+        },
+        async exportService() {
+            try {
+                const token = useCookie('auth-token')
+                const response = await $fetch(`${apiUrl}/entertainment/services/export/excel`, {
+                    method: 'GET',
+                    headers: { 'Authorization': `Bearer ${token.value}` },
+                })
+                return response
+            } catch (error) {
+                this.status_code = error.response.status || error.statusCode || error.code || 'Unknown error'
+                Cookies.set('alert-message', 'Failed to export data entertainment service')
+                Cookies.set('alert-type', 'false')
+                Cookies.set('alert-page', 'Service')
             }
         },
         async createService(createData: any) {
-            try {
-                const token = useCookie('auth-token')
-                const response = await fetch(`${apiUrl}/entertainment/services`, {
-                    method: 'POST',
-                    headers: {
-                        'Authorization': `Bearer ${token.value}`,
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(createData)
-                });
-                console.log(response)
-                this.status_code = response?.status;
-                this.totalPages = response?.meta?.total;
-            } catch (error) {
-                console.log(error?.message);
+            const token = useCookie('auth-token')
+            const response = await fetch(`${apiUrl}/entertainment/services`, {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${token.value}`,
+                },
+                body: JSON.stringify(createData)
+            })
+            this.status_code = response.status
+            if (this.status_code === 201) {
+                this.service = response.data
+                await this.getAllService()
+            } else {
+                Cookies.set('alert-message', 'Failed to create new entertainment service')
+                Cookies.set('alert-type', 'false')
+                Cookies.set('alert-page', 'Service')
             }
         },
         async saveImageService(formData: FormData, serviceId: string) {
@@ -75,10 +98,13 @@ export const useServiceStore = defineStore('service', {
                     method: 'POST',
                     headers: { 'Authorization': `Bearer ${token.value}` },
                     body: formData
-                });
-                this.status_code = response?.data ? 200 : null;
+                })
+                this.status_code = response.meta.code
             } catch (error) {
-                console.log(error?.message)
+                this.status_code = error.response.status || error.statusCode || error.code || 'Unknown error'
+                Cookies.set('alert-message', 'Failed to save image entertainment service')
+                Cookies.set('alert-type', 'false')
+                Cookies.set('alert-page', 'Service')
             }
         },
         async updateService(updateData: any, serviceId: string) {
@@ -88,26 +114,30 @@ export const useServiceStore = defineStore('service', {
                     method: 'PATCH',
                     headers: { 'Authorization': `Bearer ${token.value}` },
                     body: updateData
-                });
-                this.status_code = response?.data ? 200 : null;
+                })
+                this.status_code = response.meta.code
             } catch (error) {
-                console.log(error?.message)
+                this.status_code = error.response.status || error.statusCode || error.code || 'Unknown error'
+                Cookies.set('alert-message', 'Failed to update entertainment service')
+                Cookies.set('alert-type', 'false')
+                Cookies.set('alert-page', 'Service')
             }
         },
         async deleteService(serviceId: number) {
-            try {
-                const token = useCookie('auth-token')
-                const response = await fetch(`${apiUrl}/entertainment/services/${serviceId}`, {
-                    method: 'DELETE',
-                    headers: { 'Authorization': `Bearer ${token.value}` },
-                })
-                this.status_code = response.status;
+            const token = useCookie('auth-token')
+            const response = await fetch(`${apiUrl}/entertainment/services/${serviceId}`, {
+                method: 'DELETE',
+                headers: { 'Authorization': `Bearer ${token.value}` },
+            })
+            this.status_code = response.status
+            if (this.status_code === 200) {
                 this.page = 1
-                this.totalPages = response?.meta?.total
-                await this.getAllService();
-            } catch (error) {
-                console.log(error?.message)
+                await this.getAllService()
+            } else {
+                Cookies.set('alert-message', 'Failed to delete entertainment service')
+                Cookies.set('alert-type', 'false')
+                Cookies.set('alert-page', 'Service')
             }
         },
     },
-});
+})
