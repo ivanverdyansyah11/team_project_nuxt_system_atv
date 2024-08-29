169ff31ca91d664808322f122aeb86d411d1b0e6
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-08-29 20:02:46 +0800

    Progress: Update homepage booking success page

--- pages/homepage/booking/success.vue
+++ pages/homepage/booking/success.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
+import Cookies from "js-cookie";
+
 definePageMeta({
   layout: 'homepage'
 })
 
 const user = useCookie('auth-user')
+const bookingType = useCookie('booking-type')
+const bookingId = useCookie('booking-id')
+
+onBeforeRouteLeave((to, from, next) => {
+  Cookies.remove('booking-type')
+  Cookies.remove('booking-id')
+  next()
+})
+
+onBeforeRouteUpdate((to, from, next) => {
+  Cookies.remove('booking-type')
+  Cookies.remove('booking-id')
+  next()
+})
 </script>
 
 <template>
@@ -13,7 +29,7 @@ const user = useCookie('auth-user')
         <div class="col-12 col-md-10 col-lg-6 col-xxl-5 d-flex align-items-center justify-content-center flex-column gap-4">
           <img src="~/assets/image/other/success-image.svg" alt="Success Image">
           <div class="button-group d-flex align-items-center gap-2 mt-3">
-            <NuxtLink :to="{path: user.user.role == 'customer' ? `/homepage/profile/history-booking` : `/dashboard/history-booking`}" class="button-primary-small">History Booking</NuxtLink>
+            <NuxtLink :to="{path: user.user.role == 'customer' ? bookingType == 'service' ? `/homepage/profile/history-booking/service` : `/homepage/profile/history-booking/package` : bookingType == 'service' ? `/dashboard/history-booking/service/${bookingId}` : `/dashboard/history-booking/package/${bookingId}`}" class="button-primary-small">History Booking</NuxtLink>
             <NuxtLink :to="{path: '/homepage'}" class="button-reverse">Back to Home</NuxtLink>
           </div>
         </div>
