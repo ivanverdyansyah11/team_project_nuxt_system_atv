860fc71b7d356e6218f792632200e1d5246b3077
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-08-29 09:36:07 +0800

    Progress: Update dashboard entertainment package create page

--- pages/dashboard/entertainment-package/create.vue
+++ pages/dashboard/entertainment-package/create.vue
@@ -1,81 +1,80 @@
 <script setup lang="ts">
-import { useBundleStore } from "~/stores/bundle";
-import { useServiceStore } from "~/stores/service";
-import { useField, useForm } from 'vee-validate';
-import * as yup from 'yup';
-import { navigateTo } from "nuxt/app";
-import Cookies from "js-cookie";
-import { onMounted, ref } from 'vue';
+import {useBundleStore} from "~/stores/bundle"
+import {useServiceStore} from "~/stores/service"
+import {useField, useForm} from 'vee-validate'
+import {navigateTo} from "nuxt/app"
+import {onMounted, ref} from 'vue'
+import Cookies from "js-cookie"
+import * as yup from 'yup'
 
 definePageMeta({
   title: 'Create Package Page',
   layout: 'dashboard'
-});
+})
 
-const bundleStore = useBundleStore();
-const serviceStore = useServiceStore();
-const createDataImage = ref('https://placehold.co/600x400?text=Image+Not+Found');
-const file = ref(null);
+const bundleStore = useBundleStore()
+const serviceStore = useServiceStore()
+const createDataImage = ref('https://placehold.co/600x400?text=Image+Not+Found')
+const file = ref(null)
 
 const schema = yup.object({
   name: yup.string().required('Name is required'),
   description: yup.string().required('Description is required'),
   price: yup.number().required('Price is required'),
+  duration: yup.number().required('Duration is required'),
   expired_at: yup.date().required('Expired date is required').typeError('Invalid date format'),
-  services: yup.array().min(1, 'At least one service is required'),
   image: yup.mixed().required('Image is required'),
-});
+})
 
-const { handleSubmit, resetForm, setValues } = useForm({
+const { handleSubmit } = useForm({
   validationSchema: schema,
-});
+})
 
-const { value: name, errorMessage: nameError } = useField('name');
-const { value: description, errorMessage: descriptionError } = useField('description');
-const { value: price, errorMessage: priceError } = useField('price');
-const { value: expired_at, errorMessage: expiredAtError } = useField('expired_at');
-const { value: image, errorMessage: imageError } = useField('image');
-const services = ref([]);
-const servicesError = ref('');
+const { value: name, errorMessage: nameError } = useField('name')
+const { value: description, errorMessage: descriptionError } = useField('description')
+const { value: price, errorMessage: priceError } = useField('price')
+const { value: duration, errorMessage: durationError } = useField('duration')
+const { value: expired_at, errorMessage: expiredAtError } = useField('expired_at')
+const { value: image, errorMessage: imageError } = useField('image')
 
 const previewImage = (e: any) => {
-  file.value = e.target.files[0];
-  image.value = file.value;
-  if (!file.value) return;
-  const reader = new FileReader();
+  file.value = e.target.files[0]
+  image.value = file.value
+  if (!file.value) return
+  const reader = new FileReader()
   reader.onload = () => {
     if (typeof reader.result === "string") {
-      createDataImage.value = reader.result;
+      createDataImage.value = reader.result
     }
-    e.target.value = "";
-  };
-  reader.readAsDataURL(file.value);
-};
+    e.target.value = ""
+  }
+  reader.readAsDataURL(file.value)
+}
 
 const createBundle = handleSubmit(async (values) => {
-  values.services = services.value.map(entertainment_service_id => ({ entertainment_service_id }));
-  values.expired_at = new Date(values.expired_at).toISOString();
+  values.expired_at = new Date(values.expired_at).toISOString()
+  const { image, ...valueData } = values
   try {
-    await bundleStore.createBundle(values);
-    if (bundleStore.status_code === 200 && file.value) {
-      await bundleStore.getAllBundleWithoutPagination();
-      const formData = new FormData();
-      formData.append('image', file.value);
-      await bundleStore.saveImageBundle(formData, bundleStore.bundleAll[0].id);
-      Cookies.set('alert-message', 'Successfully create new package');
-      Cookies.set('alert-page', 'Package');
-      navigateTo('/dashboard/entertainment-package');
+    await bundleStore.createBundle(valueData)
+    if (bundleStore.status_code === 201 && file.value) {
+      const formData = new FormData()
+      formData.append('image', file.value)
+      await bundleStore.saveImageBundle(formData, bundleStore.bundle.id)
+      Cookies.set('alert-message', 'Successfully create new entertainment package')
+      Cookies.set('alert-type', 'true')
+      Cookies.set('alert-page', 'Package')
+      navigateTo('/dashboard/entertainment-package')
     } else {
-      console.error('Failed to create bundle', bundleStore.status_code);
+      navigateTo('/dashboard/entertainment-package')
     }
   } catch (error) {
-    console.error('Error creating bundle:', error);
+    navigateTo('/dashboard/entertainment-package')
   }
-});
+})
 
 onMounted(async () => {
-  await serviceStore.getAllServiceWithoutPagination();
-});
+  await serviceStore.getAllServiceWithoutPagination()
+})
 </script>
 
 <template>
@@ -96,7 +95,7 @@ onMounted(async () => {
                   <p class="invalid-label">{{ imageError }}</p>
                 </div>
               </div>
-              <div class="col-12">
+              <div class="col-md-6">
                 <div class="input-group d-flex flex-column">
                   <label for="name">Name</label>
                   <input type="text" class="input w-100" name="name" id="name"
@@ -114,22 +113,18 @@ onMounted(async () => {
               </div>
               <div class="col-md-6">
                 <div class="input-group d-flex flex-column">
+                  <label for="duration">Duration</label>
+                  <input type="number" class="input w-100" name="duration" id="duration"
+                         placeholder="Enter your duration.." autocomplete="off" v-model="duration">
+                  <p v-if="durationError" class="invalid-label">{{ durationError }}</p>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="input-group d-flex flex-column">
                   <label for="expired_at">Expired Date</label>
                   <input type="datetime-local" class="input w-100" name="expired_at" id="expired_at"
                          placeholder="Enter your expired date.." autocomplete="off" v-model="expired_at">
                   <p v-if="expiredAtError" class="invalid-label">{{ expiredAtError }}</p>
-                </div>
-              </div>
-              <div class="col-12">
-                <div class="input-group d-flex flex-column">
-                  <label>Services</label>
-                  <div class="wrapper-checkbox d-flex gap-2 flex-wrap">
-                    <div class="checkbox d-flex align-items-center gap-2 me-2" v-for="(service, index) in serviceStore.serviceAll" :key="index">
-                      <input type="checkbox" :id="service.id" :value="service.id" v-model="services">
-                      <label class="mb-0" :for="service.id">{{service.name}}</label>
-                    </div>
-                  </div>
-                  <p v-if="servicesError" class="invalid-label">{{ servicesError }}</p>
                 </div>
               </div>
               <div class="col-12">
