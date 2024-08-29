90e264dcff7ad63c56dbf1216ae7f8e3697b2cc0
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-08-29 09:38:15 +0800

    Progress: Update homepage package detail page

--- pages/homepage/package/[id].vue
+++ pages/homepage/package/[id].vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
-import { useBundleStore } from "~/stores/bundle";
-import { useRoute } from '#app';
+import {useBundleStore} from "~/stores/bundle"
+import {useRoute} from '#app'
+import {useAuthStore} from "~/stores/auth"
+import imageNotFound from '~/assets/image/other/image-not-found.svg'
 
 definePageMeta({
   layout: 'homepage'
 })
 
 const bundleStore = useBundleStore()
-const route = useRoute();
+const authStore = useAuthStore()
+const route = useRoute()
 await bundleStore.getBundleById(route.params.id)
 </script>
 
@@ -19,15 +22,15 @@ await bundleStore.getBundleById(route.params.id)
         <div class="wrapper-banner position-relative">
           <img src="~/assets/image/other/other-top.svg" alt="Other Top Image">
           <img src="~/assets/image/other/other-bottom.svg" alt="Bottom Top Image">
-          <img :src="bundleStore.bundle.image_path != null ? `http://localhost:8000/${bundleStore.bundle.image_path}` : 'https://placehold.co/600x400?text=Image+Not+Found'" alt="Banner Image" class="img-fluid position-relative banner-image">
+          <img :src="bundleStore.bundle.image_path != null ? `http://localhost:8000/${bundleStore.bundle.image_path}` : imageNotFound" alt="Banner Image" class="img-fluid position-relative banner-image">
         </div>
       </div>
       <div class="offset-lg-1 col-lg-6">
-        <h2 class="title">{{bundleStore.bundle.name}}</h2>
+        <h2 class="title mb-2">{{bundleStore.bundle.name}}</h2>
         <p class="description">{{bundleStore.bundle.description}}</p>
         <div class="button-group d-flex gap-2 align-items-center">
           <NuxtLink to="#service" class="button-primary-small">Learn More</NuxtLink>
-          <NuxtLink :to="{path: `/homepage/booking/package/${bundleStore.bundle.id}`}" class="button-reverse">Booking Now</NuxtLink>
+          <NuxtLink v-if="authStore.isLoggedIn && authStore.user.user.role != 'instructor'" :to="{path: `/homepage/booking/package/${bundleStore.bundle.id}`}" class="button-reverse">Booking Now</NuxtLink>
         </div>
       </div>
     </div>
@@ -46,10 +49,9 @@ await bundleStore.getBundleById(route.params.id)
       <div class="row row-cols-2 row-cols-md-4 row-cols-xl-5 g-3 g-md-4 content-gap content-value">
         <div v-for="(service, index) in bundleStore.bundle.services" :key="index" class="col">
           <div class="card-service">
-            <img :src="service.entertainment_service.image_path != null ? `http://localhost:8000/${service.entertainment_service.image_path}` : 'https://placehold.co/600x400?text=Image+Not+Found'" alt="">
+            <img :src="service.entertainment_service.image_path != null ? `http://localhost:8000/${service.entertainment_service.image_path}` : imageNotFound" alt="">
             <h6>{{service.entertainment_service.name}}</h6>
-            <p>Here, we offer ATV Extreme Service where you can explore...</p>
-<!--            <p>{{service.entertainment_service.description}}</p>-->
+            <p>{{service.entertainment_service.description}}</p>
             <NuxtLink :to="{path: `/homepage/service/${service.entertainment_service.id}`}" class="button-reverse w-100 text-center">Service Details</NuxtLink>
           </div>
         </div>
@@ -60,5 +62,4 @@ await bundleStore.getBundleById(route.params.id)
 </template>
 
 <style scoped>
-
 </style>
