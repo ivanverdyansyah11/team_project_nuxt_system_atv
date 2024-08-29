799cb7605f2849e8333e5b64f64ba88518db164b
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-08-29 09:37:11 +0800

    Progress: Update stores auth

--- stores/auth.ts
+++ stores/auth.ts
@@ -1,8 +1,6 @@
-// stores/auth.ts
-import { defineStore } from 'pinia';
-import { apiUrl } from '~/helpers/Variable';
-import axios from 'axios';
-import Cookies from 'js-cookie';
+import { defineStore } from 'pinia'
+import { apiUrl } from '~/helpers/Variable'
+import Cookies from 'js-cookie'
 
 export const useAuthStore = defineStore('auth', {
     state: () => ({
@@ -19,14 +17,17 @@ export const useAuthStore = defineStore('auth', {
                 const response = await $fetch(`${apiUrl}/auth/login`, {
                     method: 'POST',
                     body: credentials
-                });
-                this.token = response?.data.token;
-                this.user = response?.data;
-                Cookies.set('auth-token', this.token);
-                Cookies.set('auth-user', JSON.stringify(this.user));
-                this.status_code = response?.data ? 200 : null;
+                })
+                this.token = response.data.token
+                this.user = response.data
+                Cookies.set('auth-token', this.token)
+                Cookies.set('auth-user', JSON.stringify(this.user))
+                this.status_code = response.meta.code
             } catch (error) {
-                console.log(error.message)
+                this.status_code = error.response?.status || error.statusCode || error.code || 'Unknown error'
+                Cookies.set('alert-message', 'Email or password not found')
+                Cookies.set('alert-type', 'false')
+                Cookies.set('alert-page', 'Login')
             }
         },
         async register(registerData: any) {
@@ -34,10 +35,17 @@ export const useAuthStore = defineStore('auth', {
                 const response = await $fetch(`${apiUrl}/auth/register`, {
                     method: 'POST',
                     body: registerData,
-                });
-                this.status_code = response?.data ? 200 : null;
+                })
+                this.status_code = response.meta.code
             } catch (error) {
-                console.log(error.message)
+                this.status_code = error.response?.status || error.statusCode || error.code || 'Unknown error'
+                if (this.status_code === 400) {
+                    Cookies.set('alert-message', 'Account is already exist')
+                } else {
+                    Cookies.set('alert-message', 'Failed to register account')
+                }
+                Cookies.set('alert-type', 'false')
+                Cookies.set('alert-page', 'Register')
             }
         },
         async checkProfile() {
@@ -46,11 +54,14 @@ export const useAuthStore = defineStore('auth', {
                 const response = await $fetch(`${apiUrl}/auth/user/me`, {
                     method: 'GET',
                     headers: { 'Authorization': `Bearer ${token.value}` },
-                });
-                this.user = response?.data;
-                Cookies.set('auth-user', JSON.stringify(this.user));
+                })
+                this.user = response.data
+                Cookies.set('auth-user', JSON.stringify(this.user))
             } catch (error) {
-                console.log(error.message)
+                this.status_code = error.response?.status || error.statusCode || error.code || 'Unknown error'
+                Cookies.set('alert-message', 'Failed to check profile')
+                Cookies.set('alert-type', 'false')
+                Cookies.set('alert-page', 'Profile')
             }
         },
         async updateProfile(updateData: any) {
@@ -60,10 +71,13 @@ export const useAuthStore = defineStore('auth', {
                     method: 'PATCH',
                     headers: { 'Authorization': `Bearer ${token.value}` },
                     body: updateData
-                });
-                this.status_code = response?.meta.code;
+                })
+                this.status_code = response.meta.code
             } catch (error) {
-                console.log(error?.message)
+                this.status_code = error.response?.status || error.statusCode || error.code || 'Unknown error'
+                Cookies.set('alert-message', 'Failed to update profile')
+                Cookies.set('alert-type', 'false')
+                Cookies.set('alert-page', 'Profile')
             }
         },
         async saveImageProfile(formData: FormData) {
@@ -73,17 +87,20 @@ export const useAuthStore = defineStore('auth', {
                     method: 'POST',
                     headers: { 'Authorization': `Bearer ${token.value}` },
                     body: formData
-                });
-                this.status_code = response?.data ? 200 : null;
+                })
+                this.status_code = response.meta.code
             } catch (error) {
-                console.log(error?.message)
+                this.status_code = error.response?.status || error.statusCode || error.code || 'Unknown error'
+                Cookies.set('alert-message', 'Failed to save image profile')
+                Cookies.set('alert-type', 'false')
+                Cookies.set('alert-page', 'Profile')
             }
         },
         async logout() {
-            this.token = '';
-            this.user = null;
-            Cookies.remove('auth-token');
-            Cookies.remove('auth-user');
+            this.token = ''
+            this.user = null
+            Cookies.remove('auth-token')
+            Cookies.remove('auth-user')
         },
     },
-});
+})
