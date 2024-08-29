41e230e638cbd9d604239bda9236ce1cfb7628b7
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-08-29 09:23:15 +0800

    Progress: Update stores route

--- stores/route.ts
+++ stores/route.ts
@@ -1,5 +1,6 @@
-import { defineStore } from 'pinia';
-import { apiUrl } from '~/helpers/Variable';
+import {defineStore} from 'pinia'
+import {apiUrl} from '~/helpers/Variable'
+import Cookies from "js-cookie"
 
 export const useRouteStore = defineStore('route', {
     state: () => ({
@@ -15,26 +16,32 @@ export const useRouteStore = defineStore('route', {
         async getAllRouteWithoutPaginate() {
             try {
                 const token = useCookie('auth-token')
-                const response = await $fetch(`${ apiUrl }/routes`, {
+                const response = await $fetch(`${apiUrl}/routes`, {
                     method: 'GET',
                     headers: { 'Authorization': `Bearer ${token.value}` },
                 })
-                this.routeAll = response?.data ? response?.data : []
+                this.routeAll = response.data
             } catch (error) {
-                console.log(error?.message)
+                this.status_code = error.response.status || error.statusCode || error.code || 'Unknown error'
+                Cookies.set('alert-message', 'Failed to load data route')
+                Cookies.set('alert-type', 'false')
+                Cookies.set('alert-page', 'Route')
             }
         },
         async getAllRoute() {
             try {
                 const token = useCookie('auth-token')
-                const response = await $fetch(`${ apiUrl }/routes?current_page=${this.page}&page_size=${this.pageSize}&search=${this.keyword}`, {
+                const response = await $fetch(`${apiUrl}/routes?current_page=${this.page}&page_size=${this.pageSize}&search=${this.keyword}`, {
                     method: 'GET',
                     headers: { 'Authorization': `Bearer ${token.value}` },
                 })
-                this.routeAll = response?.data ? response?.data : []
-                this.totalPages = response?.meta?.total
+                this.routeAll = response.data
+                this.totalPages = response.meta.total
             } catch (error) {
-                console.log(error?.message)
+                this.status_code = error.response.status || error.statusCode || error.code || 'Unknown error'
+                Cookies.set('alert-message', 'Failed to load data route')
+                Cookies.set('alert-type', 'false')
+                Cookies.set('alert-page', 'Route')
             }
         },
         async getRouteById(routeId: string) {
@@ -44,9 +51,27 @@ export const useRouteStore = defineStore('route', {
                     method: 'GET',
                     headers: { 'Authorization': `Bearer ${token.value}` },
                 })
-                this.route = response?.data ? response?.data : {}
+                this.route = response.data
             } catch (error) {
-                console.log(error?.message)
+                this.status_code = error.response.status || error.statusCode || error.code || 'Unknown error'
+                Cookies.set('alert-message', 'Failed to load data route')
+                Cookies.set('alert-type', 'false')
+                Cookies.set('alert-page', 'Route')
+            }
+        },
+        async exportRoute() {
+            try {
+                const token = useCookie('auth-token')
+                const response = await $fetch(`${apiUrl}/routes/export/excel`, {
+                    method: 'GET',
+                    headers: { 'Authorization': `Bearer ${token.value}` },
+                })
+                return response
+            } catch (error) {
+                this.status_code = error.response.status || error.statusCode || error.code || 'Unknown error'
+                Cookies.set('alert-message', 'Failed to export data route')
+                Cookies.set('alert-type', 'false')
+                Cookies.set('alert-page', 'Route')
             }
         },
         async createRoute(createData: any) {
@@ -56,41 +81,47 @@ export const useRouteStore = defineStore('route', {
                     method: 'POST',
                     headers: { 'Authorization': `Bearer ${token.value}` },
                     body: createData
-                });
-                this.status_code = response?.data ? 200 : null;
-                this.totalPages = response?.meta?.total
+                })
+                this.status_code = response.meta.code
+                this.totalPages = response.meta.total
             } catch (error) {
-                console.log(error?.message)
+                this.status_code = error.response.status || error.statusCode || error.code || 'Unknown error'
+                Cookies.set('alert-message', 'Failed to create new route')
+                Cookies.set('alert-type', 'false')
+                Cookies.set('alert-page', 'Route')
             }
         },
         async updateRoute(updateData: any, routeId: string) {
             try {
-                console.log(routeId)
                 const token = useCookie('auth-token')
                 const response = await $fetch(`${apiUrl}/routes/${routeId}`, {
                     method: 'PATCH',
                     headers: { 'Authorization': `Bearer ${token.value}` },
                     body: updateData
-                });
-                this.status_code = response?.data ? 200 : null;
+                })
+                this.status_code = response.meta.code
             } catch (error) {
-                console.log(error?.message)
+                this.status_code = error.response.status || error.statusCode || error.code || 'Unknown error'
+                Cookies.set('alert-message', 'Failed to update route')
+                Cookies.set('alert-type', 'false')
+                Cookies.set('alert-page', 'Route')
             }
         },
         async deleteRoute(routeId: number) {
-            try {
-                const token = useCookie('auth-token')
-                const response = await fetch(`${apiUrl}/routes/${routeId}`, {
-                    method: 'DELETE',
-                    headers: { 'Authorization': `Bearer ${token.value}` },
-                })
-                this.status_code = response.status;
+            const token = useCookie('auth-token')
+            const response = await fetch(`${apiUrl}/routes/${routeId}`, {
+                method: 'DELETE',
+                headers: { 'Authorization': `Bearer ${token.value}` },
+            })
+            this.status_code = response.status
+            if (this.status_code === 200) {
                 this.page = 1
-                this.totalPages = response?.meta?.total
-                await this.getAllRoute();
-            } catch (error) {
-                console.log(error?.message)
+                await this.getAllRoute()
+            } else {
+                Cookies.set('alert-message', 'Failed to delete route')
+                Cookies.set('alert-type', 'false')
+                Cookies.set('alert-page', 'Route')
             }
         },
     },
-});
+})
